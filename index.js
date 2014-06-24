4005221660c564e86f4f7e59a6a24eeecc0ea197
Alex Robson <asrobson@gmail.com>
2014-06-24 11:24:30 -0500

    Minor refactor

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
-var fs = require("fs");
-var path = require("path");
+var fs = require( "fs" );
+var path = require( "path" );
 
 var banners = {};
 
-function addPath(dir) {
-	var joined = arguments.length == 1 ? dir : path.join.apply( null, Array.prototype.slice.call( arguments, 0 ) );
-	var full = path.resolve(joined);
-	fs.readdirSync(full).forEach(function(file) {
-	    registerBanner(file.slice(0, -3), require(path.join(full, file)));
-	});
+function addPath( dir ) {
+	var args = Array.prototype.slice.call( arguments, 0 );
+	var joined = arguments.length == 1 ? dir : path.join.apply( null, args );
+	var full = path.resolve( joined );
+	fs.readdirSync( full ).forEach( function( file ) {
+		registerBanner( file.slice( 0, -3 ), require( path.join( full, file ) ) );
+	} );
 	return banners;
 }
 
-function registerBanner(name, banner) {
-	banners[name] = banner;
+function registerBanner( name, banner ) {
+	banners[ name ] = banner;
 	return banners;
 }
 
-addPath(__dirname, "art/");
+addPath( __dirname, "art/" );
 banners.addPath = addPath;
 banners.registerBanner = registerBanner;
 
